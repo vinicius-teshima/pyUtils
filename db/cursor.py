d15caf7b4bc9422bbae0cbb361a4f5f594156e26
Vinicius Teshima <viniciusteshima@nasajon.com.br>
2025-04-11 19:11:16 -0300

    Fixing fetchall/fetchone when conn returns RealDict

--- db/cursor.py
+++ db/cursor.py
@@ -3,27 +3,23 @@ import typing as ty
 from .. import may_throw, typs, exceptions
 
 def fetchall(cur: typs.CurTy) -> typs.MayErrTy[typs.ListDictTy[ty.Any]]:
-    rows, err = may_throw(cur.fetchall)
+    res, err = may_throw(cur.fetchall)
     if err is not None:
         return [], err
 
-    if rows is None or len(rows) == 0:
+    if res is None or len(res) == 0:
         return [], exceptions.EmptyResultError('Nenhum Resultado da query.')
 
-    assert cur.description is not None
-    cols: ty.List[str] = [x.name for x in cur.description]
-    return [dict(zip(cols, x)) for x in rows], None
+    return ty.cast(typs.ListDictTy[ty.Any], res), None
 
 def fetchone(cur: typs.CurTy) -> typs.MayErrTy[typs.DictTy[ty.Any]]:
-    row, err = may_throw(cur.fetchone)
+    res, err = may_throw(cur.fetchone)
     if err is not None:
         return {}, err
-    assert row is None \
-           or isinstance(row, tuple)
+    assert res is None \
+           or isinstance(res, dict)
 
-    if row is None or len(row) == 0:
+    if res is None or len(res) == 0:
         return {}, exceptions.EmptyResultError('Nenhum Resultado da query.')
 
-    assert cur.description is not None
-    cols: ty.List[str] = [x.name for x in cur.description]
-    return dict(zip(cols, row)), err
+    return res, err
