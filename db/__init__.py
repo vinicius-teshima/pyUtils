840d68c6e263cb11d804e7c44c040e7d14a0a2b8
Vinicius Teshima <viniciusteshima@nasajon.com.br>
2025-04-11 18:50:44 -0300

    Addint conn_creator_closure

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,11 @@
-from . import execute, cursor, get
+import typing as ty
+
+from . import execute, cursor
+
+from .. import typs
+
+import psycopg2
+import psycopg2.extras
 
 class DBInvalidStateError(Exception):
     def __init__(self, msg: str):
@@ -6,9 +13,21 @@ class DBInvalidStateError(Exception):
         pass
     pass
 
+def conn_creator_closure(database: str, user: str,
+                         password: str, host: str,
+                         port: str) -> ty.Callable[[], typs.ConnTy]:
+    def conn_creator() -> typs.ConnTy:
+        conn = psycopg2.connect(dbname=database, user=user,
+                                password=password, host=host,
+                                port=port,
+                                cursor_factory=psycopg2.extras.RealDictCursor)
+        psycopg2.extras.register_uuid(conn_or_curs=conn)
+        return conn
+    return conn_creator
+
 __all__ = [
     'DBInvalidStateError',
     'execute',
     'cursor',
-    'get'
+    'conn_creator_closure',
 ]
